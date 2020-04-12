2d86ef4190bdcc18af3eac2bcdd85c1fa059db51
varjunan <varjunan@roku.com>
2020-04-11 19:00:59 -0700

    fixed no school selected

--- ui/src/Main.js
+++ ui/src/Main.js
@@ -14,7 +14,7 @@ function Main() {
             <Header/>
             <Introduce/>
             <School/>
-            <FeaturedProducts/>
+            {/* <FeaturedProducts/> */}
             <Footer/>
         </div>
     );
--- ui/src/store/reducers/schoolReducer.js
+++ ui/src/store/reducers/schoolReducer.js
@@ -11,21 +11,28 @@ const initialState ={
 const schoolReducer = function (state = initialState, action) {
     switch(action.type) {
         case Actions.GET_ALL_PRODUCTS: {
-            var productDetailList = [
-                {
-                    title: "Need of Chairs",
-                    name: "Steel Chairs",
-                    counts: 15,
-                    postedAt: '07/08/2019',
-                    parentId: 3,
-                    url: "https://www.amazon.in/bi3-Portable-Seating-Multipurpose-Folding/dp/B07PQ83G39/ref=sr_1_6?qid=1562582426&refinements=p_36%3A3444811031&s=kitchen&sr=1-6",
-                    image: "chair.jpg",
-                    description: "Lightweight, portable and folds flat for easy storage. Large and comfortable padding.Functional Comfortable Design",
-                    dist: "tirunelVeli"
+            var allProductsList = []
+            if ( action.payload.length > 0) {
+                for ( var index = 0; index < action.payload.length; index++) {
+                    allProductsList.push({ 
+                        title: action.payload[index].title,
+                        name: action.payload[index].title,
+                        counts: action.payload[index].quantity,
+                        fulfilled: action.payload[index].fulfilled_count,
+                        postedAt: action.payload[index].posted_date,
+                        parentId: 3,
+                        url: action.payload[index].url,
+                        image: "chair.jpg",
+                        description: action.payload[index].description,
+                        dist: "tirunelVeli"
+                    })
                 }
-            ]
-            return {...state,  allProducts: productDetailList}
-            //return {...state,  allProducts: action.payload}
+                console.log("all products " + allProductsList)
+                return {...state,  allProducts: allProductsList}
+            } else {
+                console.log("all products else " + allProductsList)
+                return {...state,  allProducts: action.payload}
+            }    
         }
         case Actions.GET_ALL_STATES: {
             return {...state,  states: action.payload}
@@ -39,7 +46,7 @@ const schoolReducer = function (state = initialState, action) {
             //console.log("School reducer - GET_SCHOOLS_FROM_DIST - ",action.payload[0].name)
             if ( action.payload.length > 0) {
                 var schoolListFromBackend = [];
-                for ( var index = 0; index < action.payload.length; index++) {
+                for (  index = 0; index < action.payload.length; index++) {
                     schoolListFromBackend.push(
                        {label: action.payload[index].name + "," + action.payload[index].place, 
                                 schoolId:action.payload[index].school_id , id: index},
@@ -54,12 +61,12 @@ const schoolReducer = function (state = initialState, action) {
         case Actions.GET_PRODUCTS_GROUP: {
             var productDetailList = []
             if ( action.payload.length > 0) {
-                var productDetailList = []
-                for ( var index = 0; index < action.payload.length; index++) {
+                for (  index = 0; index < action.payload.length; index++) {
                     productDetailList.push({ 
                         title: action.payload[index].title,
                         name: action.payload[index].title,
                         counts: action.payload[index].quantity,
+                        fulfilled: action.payload[index].fulfilled_count,
                         postedAt: action.payload[index].posted_date,
                         parentId: 3,
                         url: action.payload[index].url,
